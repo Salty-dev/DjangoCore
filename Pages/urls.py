2882f3a2bcdbc2a4b40cc97ddf21fc51e1cb0918
Salty-dev <saltydevelopment@protonmail.com>
2021-04-04 11:48:39 +0200

    Mistake is fixed in Pages urls

--- Pages/urls.py
+++ Pages/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 
 from Pages.views import home, contact, about
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', home, name="home"),
     path('contact/', contact, name="contact"),
     path('about/', about, name="about")
